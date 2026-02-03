e70f212694aa521e5ac3e7851102c8911d004edb
zamu <ddz0703@gmail.com>
2026-02-03 00:21:01 -0300

    Added some code in Krieg.cpp for generate a testing grid struct Point static Point GridToScreen() static void DrawIsoDiamond()

--- TankKrieg/src/Krieg.cpp
+++ TankKrieg/src/Krieg.cpp
@@ -1,6 +1,30 @@
 #include "Krieg.h"
 #include <iostream>
 
+struct Point {
+    float x, y;
+};
+
+static Point GridToScreen(int gx, int gy, int tileW, int tileH, int originX, int originY)
+{
+    float sx = originX + (gx - gy) * (tileW * 0.5f);
+    float sy = originY + (gx + gy) * (tileH * 0.5f);
+    return { sx, sy };
+}
+
+static void DrawIsoDiamond(SDL_Renderer* r, float cx, float cy, int tileW, int tileH)
+{
+    SDL_FPoint top = { cx,              cy - tileH * 0.5f };
+    SDL_FPoint right = { cx + tileW * 0.5f, cy };
+    SDL_FPoint bottom = { cx,              cy + tileH * 0.5f };
+    SDL_FPoint left = { cx - tileW * 0.5f, cy };
+
+    SDL_RenderLine(r, top.x, top.y, right.x, right.y);
+    SDL_RenderLine(r, right.x, right.y, bottom.x, bottom.y);
+    SDL_RenderLine(r, bottom.x, bottom.y, left.x, left.y);
+    SDL_RenderLine(r, left.x, left.y, top.x, top.y);
+}
+
 Krieg::Krieg() {}
 
 Krieg::~Krieg() {
@@ -60,6 +84,23 @@ void Krieg::Update(float /*deltaTime*/) {
 void Krieg::Render() {
     SDL_SetRenderDrawColor(renderer, 25, 25, 25, 255);
     SDL_RenderClear(renderer);
+
+    const int tileW = 64;
+    const int tileH = 64 / 2;
+
+    const int originX = 400; // centro de tu ventana (ajustalo)
+    const int originY = 100; // un poco abajo del top para que se vea
+
+    SDL_SetRenderDrawColor(renderer, 220, 220, 220, 255);
+
+    // dibujar una grilla 10x10
+    for (int gy = 0; gy < 10; gy++) {
+        for (int gx = 0; gx < 10; gx++) {
+            Point p = GridToScreen(gx, gy, tileW, tileH, originX, originY);
+            DrawIsoDiamond(renderer, p.x, p.y, tileW, tileH);
+        }
+    }
+
 
     SDL_RenderPresent(renderer);
 }
